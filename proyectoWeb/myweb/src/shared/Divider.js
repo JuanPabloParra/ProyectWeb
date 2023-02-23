b1b49b2303b6ce46fe2cec2a0327c8f90f93151e
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-02-22 23:31:21 -0500

    comentando funciones del codigo

--- proyectoWeb/myweb/src/shared/Divider.js
+++ proyectoWeb/myweb/src/shared/Divider.js
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/react';
-
+//este componente recibe un borde solid gray para poder hacer una division del header de la web.
 function Divider() {
   return (
     <Box as="hr" borderBottom="1px solid gray" my="0" w="90%" position="absolute" top="66px" />
