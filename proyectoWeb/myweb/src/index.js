3dac0935e5a20a6d0443c8b0eb8a9872d076578a
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-05-22 20:40:37 -0500

    firebase

--- proyectoWeb/myweb/src/index.js
+++ proyectoWeb/myweb/src/index.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import { ChakraProvider, Box, Flex, Text } from '@chakra-ui/react';
 import Menu from './shared/Menu';
 import HomePage from './pages/HomePage';
 import App from './App';
 import ChatBubble from './components/ChatBubble';
+
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-    <Flex direction="column" align="center" maxW="1200px" mx="auto" p="4">
-         <Box mt="4">
+      <Flex direction="column" align="center" maxW="1200px" mx="auto" p="4">
+        <Box mt="4">
           <Text fontSize="2xl" fontWeight="bold"></Text>
           <HomePage />
           <App />
-          <ChatBubble />
+          <ChatBubble /> {/* Agrega este componente aquí */}
         </Box>
       </Flex>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
-
-
-
